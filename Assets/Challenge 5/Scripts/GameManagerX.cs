87dba06d736e34d5a6c66e93f2c52509ab7ca9b0
Artem <gpsqaz1@gmail.com>
2024-04-21 15:34:13 +0700

    timer???

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -21,13 +21,15 @@ public class GameManagerX : MonoBehaviour
     private float spaceBetweenSquares = 2.5f; 
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
-    
+    private int time;
+
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
     public void StartGame(int difficulty)
     {
         spawnRate /= difficulty;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
+        StartCoroutine(ExecuteAfterTime());
         score = 0;
         UpdateScore(0);
         titleScreen.SetActive(false);
@@ -70,8 +72,23 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "Score:";
+        scoreText.text = "Score:" + score;
     }
+
+    /*public void TimerUpdate()
+    {
+       for (int i = 0; i < 60; i--)
+       {
+         
+       }
+    }*/
+
+    IEnumerator ExecuteAfterTime(int timeInSec = 60)
+    {
+        yield return new WaitForSeconds(timeInSec);
+        GameOver();
+    }
+
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
